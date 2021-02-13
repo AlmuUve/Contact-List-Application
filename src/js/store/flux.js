650b1b1d92bd7f29b148ff7c52d1c4db1f407347
AlmuUve <avelazqc@gmail.com>
2021-02-13 13:59:43 +0000

    adding POST, PUT and DELETE functions

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,25 +2,77 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			contacts: []
-
 		},
 		actions: {
-
-            // setUser: user => {
-			// 	setStore({ user: user });
-			// },
+			setContact: contact => {
+				setStore({ contact: contact });
+			},
 
 			getSingleContact: async id => {
-				let response = await fetch('https://assets.breatheco.de/apis/fake/contact/' + id);
+				let response = await fetch("https://assets.breatheco.de/apis/fake/contact/" + id);
 				response = await response.json();
-				getActions().setUser(response);
-            },
-            
+				getActions().setContact(response);
+			},
+
 			getAllContacts: async () => {
-				let response = await fetch('https://assets.breatheco.de/apis/fake/contact/agenda/Almu_Agenda');
+				let response = await fetch("https://assets.breatheco.de/apis/fake/contact/agenda/Almu_Agenda");
 				response = await response.json();
 				setStore({ contacts: response });
 			},
+
+			createContact: async data => {
+				let response = await fetch("https://assets.breatheco.de/apis/fake/contact/", {
+					method: "POST",
+					mode: "cors",
+					redirect: "follow",
+					headers: new Headers({
+						"Content-Type": "application/json"
+					}),
+					body: JSON.stringify({
+						name: data.name,
+						email: data.email,
+						agenda_slug: "Almu_Agenda",
+						address: data.address,
+						phone: data.phone
+					})
+				});
+				response = await response.json();
+				getActions().getAllContacts();
+			},
+
+			deleteContact: async id => {
+				let response = await fetch("https://assets.breatheco.de/apis/fake/contact/" + id, {
+					method: "DELETE",
+					mode: "cors",
+					redirect: "follow",
+					headers: new Headers({
+						"Content-Type": "application/json"
+					})
+				});
+				response = await response.json();
+				getActions().getAllContacts();
+			},
+
+			updateContact: async (id, field) => {
+				let response = await fetch("https://assets.breatheco.de/apis/fake/contact/" + id, {
+					method: "PUT",
+					mode: "cors",
+					redirect: "follow",
+					headers: new Headers({
+						"Content-Type": "application/json"
+					}),
+					body: JSON.stringify({
+						name: field.name,
+						email: field.email,
+						agenda_slug: "Almu_Agenda",
+						address: field.address,
+						phone: field.phone
+					})
+				});
+				response = await response.json();
+				getActions().getAllContacts();
+				getActions().setContact("");
+			}
 		}
 	};
 };

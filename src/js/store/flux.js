1199309b57dc4b31bc794ddf7a6a48d38198bd3d
AlmuUve <avelazqc@gmail.com>
2021-02-13 10:51:04 +0000

    adding GET functions

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,26 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			contacts: []
+
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+            // setUser: user => {
+			// 	setStore({ user: user });
+			// },
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			getSingleContact: async id => {
+				let response = await fetch('https://assets.breatheco.de/apis/fake/contact/' + id);
+				response = await response.json();
+				getActions().setUser(response);
+            },
+            
+			getAllContacts: async () => {
+				let response = await fetch('https://assets.breatheco.de/apis/fake/contact/agenda/Almu_Agenda');
+				response = await response.json();
+				setStore({ contacts: response });
+			},
 		}
 	};
 };
